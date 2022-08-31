270942c97ac6eae472ac797ba9e948ce92fe82fa
Wendy <112121993+WinnieDixie@users.noreply.github.com>
2022-08-31 10:43:26 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,11 @@
+import sys
+
+first_number = 3
+second_number = 2
+result_sum = first_number + second_number
+
+def add_numbers(first_number,second_number):
+    result_sum = first_number + second_number
+    return result_sum
+
+print(add_numbers(first_number, second_number))
