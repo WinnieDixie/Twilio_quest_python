a5552663d93964abce1e8bee001e8759a86b02d7
Wendy <112121993+WinnieDixie@users.noreply.github.com>
2022-08-31 10:39:32 -0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,10 @@
+import sys
+
+input_string = sys.argv.upper()
+output_string = input_string
+
+part_one = (output_string)
+part_two = "!!!"
+
+full_message = part_one + part_two
+print(full_message)
